ca9a642611965a379f6de2d88ec6213b08e6c5cf
MasonCathorall <masoncathorallrev@gmail.com>
2023-03-21 10:11:07 -0400

    Challenge finished

---
+++ app/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodingChallenge
+{
+    class Program
+    {
+        public static bool CheckEquality(object a, object b){
+            return a.Equals(b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing 1 and 1: Expect true");
+            Console.WriteLine(CheckEquality(1, 1));
+
+            Console.WriteLine("Testing 1 and \"1\": Expect false");
+            Console.WriteLine(CheckEquality(1, "1"));
+
+            Console.WriteLine("Testing 1 and true: Expect false");
+            Console.WriteLine(CheckEquality(1, true));
+        }
+    }
+}
